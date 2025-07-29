910b3ad47858d551345e49482b733f3fa752d103
Harsh <userharsh911@gmail.com>
2025-07-29 17:29:22 +0530

    completed  commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import {useSelector} from "react-redux"
-import {useNavigate} from "react-router-dom"
+import {useLocation, useNavigate} from "react-router-dom"
 import Button from '../Button'
 import LogoutBtn from '../LogoutBtn'
 import Admin from '../Admin/Admin'
 const Header = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const {pathname} = useLocation();
     const [isDark,setIsDark] = useState()
     const authStatus = useSelector(state => state.isAuthenticated)
     const user = useSelector(state => state.user)
     
+    useEffect(()=>{
+        console.log("path ",pathname)
+    },[pathname])
+
     const navItems = [
         {
             name:"Home",
             navTo:'/home',
             active :true
+            // isHighLight : pathname==navTo
         },
         {
             name :"Login",
@@ -53,7 +59,12 @@ return (
         <div className="container mx-auto px-4">
             <div className="flex items-center justify-between h-16">
                 <div className="flex items-center space-x-4">
-                    {user ? <Admin user={user} onClick={()=>navigate(`/admin/${user.$id}`)}/> : null}
+                    {user ? (
+                        <Admin 
+                            user={user} 
+                            className={`${pathname=='/admin/'+user.$id ? 'bg-emerald-600 text-white ' : 'text-gray-700 dark:text-gray-200'}`} 
+                            onClick={()=>navigate(`/admin/${user.$id}`)}/>
+                    ) : null}
                     <button
                         onClick={darkToggle}
                         className="relative cursor-pointer w-14 h-7 rounded-full bg-gray-700 dark:bg-gray-600 flex items-center transition-colors duration-300 focus:outline-none"
@@ -81,7 +92,7 @@ return (
                                     <li key={item.name}>
                                         <Button
                                             onClick={() => navigate(item.navTo)}
-                                            className="px-4 py-2 cursor-pointer text-black dark:text-gray-100 hover:text-blue-400 dark:hover:text-blue-300 rounded-md transition duration-300 hover:bg-gray-200 dark:hover:bg-gray-700"
+                                            className={`px-4 py-2 ${pathname==item.navTo ? 'bg-emerald-600 text-emerald-100' : 'hover:bg-gray-200 dark:hover:bg-gray-700 hover:text-blue-400 dark:hover:text-blue-300'} cursor-pointer text-black dark:text-gray-100  rounded-md transition duration-300 `}
                                         >
                                             {item.name}
                                         </Button>
@@ -97,7 +108,7 @@ return (
                 {/* Mobile Menu Button */}
                 <button
                     onClick={toggleSidebar}
-                    className="md:hidden p-2 text-gray-600 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-100"
+                    className="md:hidden p-2 cursor-pointer text-gray-600 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-100"
                 >
                     <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         {isSidebarOpen ? (
@@ -114,7 +125,7 @@ return (
         <div className={`md:hidden fixed inset-0 z-40 transform ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out`}>
             <div className="fixed inset-0" onClick={toggleSidebar}></div>
             <div className="fixed inset-y-0 left-0 w-64 bg-zinc-200 dark:bg-gray-800 shadow-lg transform transition-transform duration-300 ease-in-out">
-                <div className="p-4">
+                <div className="p-4 flex flex-col">
                     <ul className="space-y-4">
                         {navItems.map(
                             (item) =>
@@ -125,21 +136,26 @@ return (
                                                 navigate(item.navTo);
                                                 toggleSidebar();
                                             }}
-                                            className="w-full px-4 py-2 cursor-pointer text-black dark:text-gray-100 hover:text-blue-400 dark:hover:text-blue-300 rounded-md transition duration-300 hover:bg-gray-200 dark:hover:bg-gray-700"
+                                            className={`w-full px-4 ${pathname==item.navTo ? 'bg-emerald-600 text-emerald-100' : 'hover:bg-gray-200 dark:hover:bg-gray-700 hover:text-blue-400 dark:hover:text-blue-300'} py-2 cursor-pointer text-black dark:text-gray-100  rounded-md transition duration-300 `}
                                         >
                                             {item.name}
                                         </Button>
                                     </li>
                                 )
                         )}
-                        {authStatus && (
-                            <LogoutBtn className="w-full px-4 py-2 bg-red-600 dark:bg-red-700 text-white rounded-md hover:bg-red-700 dark:hover:bg-red-800 transition duration-300 cursor-pointer" toggleSidebar={toggleSidebar} />
+                        
+                    </ul>
+                    {authStatus && (
+                            <LogoutBtn className="mx-auto px-4 py-2 my-5 bg-red-600 dark:bg-red-700 text-white rounded-md hover:bg-red-700 dark:hover:bg-red-800 transition duration-300 cursor-pointer" toggleSidebar={toggleSidebar} />
                         )}
-                        {user ? <Admin user={user} onClick={()=>{
+                        {user ? (
+                            <Admin 
+                                user={user}
+                                className={`${pathname=='/admin/'+user.$id ? 'bg-emerald-600 text-white ' : 'text-gray-700 dark:text-gray-200'}`} onClick={()=>{
                                     navigate(`/admin/${user.$id}`);
                                     toggleSidebar();
-                                }}/> : null}
-                    </ul>
+                                }}/>
+                        ) : null}
                 </div>
             </div>
         </div>
--- src/components/LogoutBtn.jsx
+++ src/components/LogoutBtn.jsx
@@ -19,7 +19,24 @@ const LogoutBtn = ({className,toggleSidebar}) => {
         })
     }
   return (
-    <button className={`${className}`} onClick={logout}>Logout</button>
+    <button className={`${className}`} onClick={logout}>
+      <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        width="24" 
+        height="24" 
+        viewBox="0 0 24 24" 
+        fill="none" 
+        stroke="currentColor" 
+        strokeWidth="2" 
+        strokeLinecap="round" 
+        strokeLinejoin="round"
+    >
+      <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
+      <path d="M10 17l5-5-5-5" />
+      <path d="M15 12H3" />
+    </svg>
+
+    </button>
   )
 }
 
